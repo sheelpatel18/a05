9e6addceb3377af7bfe1cd50a0b68cfbc4a778dd
sheelpatel <sheelu18@email.unc.edu>
2022-03-22 20:45:58 -0400

    novice

--- src/main/java/com/comp301/a05driver/ProximityIterator.java
+++ src/main/java/com/comp301/a05driver/ProximityIterator.java
@@ -3,28 +3,55 @@ package com.comp301.a05driver;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class ProximityIterator {
+public class ProximityIterator implements Iterator<Driver> {
     Iterator<Driver> driverPool;
     Position clientPosition;
     int proximityRange;
+    private Driver nextDriver;
 
     public ProximityIterator(Iterable<Driver> driverPool, Position clientPosition, int proximityRange) {
         this.driverPool = driverPool.iterator();
         this.clientPosition = clientPosition;
         this.proximityRange = proximityRange;
+        this.nextDriver = null;
     }
     
     public boolean hasNext() {
-        return driverPool.hasNext();
+        if (nextDriver != null) {
+            return true;
+        }
+        loadNext();
+        return nextDriver != null;
+    }
+
+    private void loadNext() {
+        Driver next = this.getNext();
+        if (next != null) {
+            this.nextDriver = next;
+        }
+    }
+
+    private Driver getNext() {
+        try {
+            Driver driverPoolNext = driverPool.next();
+            if (driverPoolNext.getVehicle().getPosition().getManhattanDistanceTo(clientPosition) <= proximityRange) {
+                return driverPoolNext;
+            } else {
+                return getNext();
+            }
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+        
     }
 
     public Driver next() {
-        Driver driver = driverPool.next();
-        if (driver.getVehicle().getPosition().getManhattanDistanceTo(clientPosition) <= proximityRange) {
-            return driver;
+        if (!hasNext()) {
+            throw new NoSuchElementException();
         }
-        return next();
-        // already throws NoSuchElementException in
+        Driver next = this.nextDriver;
+        loadNext();
+        return next;
     }
     
 }
