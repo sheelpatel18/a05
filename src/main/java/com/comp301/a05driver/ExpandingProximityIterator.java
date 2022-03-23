67ad0f25211f56ba2b844708c8b5006fb3a5104f
sheelpatel <sheelu18@email.unc.edu>
2022-03-22 22:22:49 -0400

    adept

---
+++ src/main/java/com/comp301/a05driver/ExpandingProximityIterator.java
@@ -0,0 +1,78 @@
+package com.comp301.a05driver;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ExpandingProximityIterator implements Iterator<Driver> {
+    Iterable<Driver> driverPool;
+    Iterator<Driver> driverPoolIterator;
+    Position clientPosition;
+    int expansionStep;
+    int lowerBound;
+    int upperBound;
+    Driver nextDriver;
+
+    public ExpandingProximityIterator(Iterable<Driver> driverPool, Position clientPosition, int expansionStep) {
+        if (!(driverPool != null && clientPosition != null && expansionStep > 0)) {
+            throw new IllegalArgumentException("Invalid arguments");
+        }
+        this.driverPool = driverPool;
+        this.driverPoolIterator = driverPool.iterator();
+        this.clientPosition = clientPosition;
+        this.expansionStep = expansionStep;
+        this.lowerBound = 0;
+        this.upperBound = 1;
+        this.nextDriver = null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (nextDriver != null) {
+            return true;
+        }
+        loadNext();
+        if (nextDriver == null) { 
+            iterate();
+            loadNext();
+        }
+        return nextDriver != null;
+    }
+
+    private void iterate() {
+        int temp = lowerBound;
+        lowerBound = upperBound;
+        upperBound = temp + expansionStep;
+    }
+
+    private void loadNext() {
+        Driver next = this.getNext();
+        if (next != null) {
+            this.nextDriver = next;
+        }
+    }
+
+    private Driver getNext() {
+        try {
+            Driver driverPoolNext = driverPoolIterator.next();
+            if (driverPoolNext.getVehicle().getPosition().getManhattanDistanceTo(clientPosition) <= upperBound && driverPoolNext.getVehicle().getPosition().getManhattanDistanceTo(clientPosition) >= lowerBound) {
+                return driverPoolNext;
+            } else {
+                return getNext();
+            }
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+
+    }
+
+    @Override
+    public Driver next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        Driver next = nextDriver;
+        nextDriver = null;
+        return next;
+    }
+
+}
